0cec3f5cf43d0159a4a17e6dad0a2a430cb2844a
Candela Dagrosa <candelada.grosa@hotmail.com>
2025-08-24 23:35:55 -0300

    Avance semana 2

---
+++ PracticaClase/Controllers/AutoController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PracticaClase.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace PracticaClase.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AutoController : ControllerBase
+    {
+        // POST api/<AutoController>
+        [HttpPost]
+        public List<Auto> GetAutos(Auto auto)
+        {
+            List<Auto> autos = new List<Auto>();
+
+            for(int i = 0; i < 10; i++)
+            {
+                autos.Add(auto);
+            }
+
+            return autos;
+        }
+    }
+}
---
+++ PracticaClase/Models/Auto.cs
@@ -0,0 +1,21 @@
+﻿namespace PracticaClase.Models
+{
+    public class Auto
+    {
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public int Anio { get; set; }
+        public string Color { get; set; }
+        public Auto(string marca, string modelo, int anio, string color)
+        {
+            Marca = marca;
+            Modelo = modelo;
+            Anio = anio;
+            Color = color;
+        }
+        public override string ToString()
+        {
+            return $"{Anio} {Marca} {Modelo} - Color: {Color}";
+        }
+    }
+}
